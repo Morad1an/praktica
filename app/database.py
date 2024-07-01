99c5124620528e829fca166d9dd185afb877eb99
Shpakoplyak <ilya2220@mail.ru>
2024-07-02 01:23:31 +0300

    Добавил новые ручки, схемы, модели, ДАО. Реализована регистрация и авторизация

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.config import settings
+DATABASE_URL = settings.DB_URL
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
---
+++ app/users/dependencies.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from fastapi import Request, HTTPException, Depends, status
+from jose import JWTError, jwt
+from app.config import settings
+from app.users.dao import UsersDAO
+
+
+def get_token(request: Request):
+    token = request.cookies.get('booking_access_token')
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Токен отсутствует")
+    return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Некорректный формат токена")
+    expire: str = payload.get("exp")
+    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Срок действия токена истек")
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user = await UsersDAO.find_by_id(int(user_id))
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return user
